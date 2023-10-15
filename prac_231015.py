8b876c4f49de9aab28c6a110729433c7f929c719
unknown <mm9326@naver.com>
2023-10-16 00:13:11 +0900

    231016

--- prac_231015.py
+++ prac_231015.py
@@ -138,6 +138,11 @@ sys.stdin = open('test.txt')
 
 # N, M = map(int, sys.stdin.readline().rstrip().split())
 # arr = list(map(int, sys.stdin.readline().split()))
+# memo = [0] * (len(arr)+1)
+# memo[1] = arr[0]
+# for k in range(2, len(arr)+1):
+#     memo[k] = memo[k-1] + arr[k-1]
+#
 # for _ in range(M):
 #     i, j = map(int, sys.stdin.readline().rstrip().split())
-#     print(sum(arr[i-1:j]))
+#     print(memo[j] - memo[i-1])
