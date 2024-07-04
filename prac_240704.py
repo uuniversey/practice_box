517ca5d6835841528bdbf09a94bb241fd4640715
mm9326 <mm9326@naver.com>
2024-07-04 21:13:21 +0900

    240704

---
+++ prac_240704.py
@@ -0,0 +1,23 @@
+
+import sys
+sys.stdin = open('test.txt')
+
+# 백준 16396. 선 그리기
+
+# N = int(input())
+# line = [0] * 10001
+# for _ in range(N):
+#     X, Y = map(int, input().split())
+#     for i in range(X, Y):
+#         line[i] = 1
+#
+# print(sum(line))
+
+
+# 백준 27294. 몇개고?
+
+# T, S = map(int, input().split())
+# if 12 <= T <= 16 and S == 0:
+#     print(320)
+# else:
+#     print(280)
